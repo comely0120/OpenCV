b34b0b3c5990edab95ea97ea7702f70533da95f9
comely0120 <65273243+comely0120@users.noreply.github.com>
2021-04-01 19:22:38 +0900

    Add files via upload

---
+++ ch02/img_info.py
@@ -0,0 +1,58 @@
+﻿import sys
+import cv2
+
+
+# 영상 불러오기
+img1 = cv2.imread('ch02\\flower.PNG', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('ch02\\flower.PNG', cv2.IMREAD_COLOR)
+
+if img1 is None or img2 is None:
+    print('Image load failed!')
+    sys.exit()
+
+# 영상의 속성 참조
+print('type(img1):', type(img1))
+print('img1.ndim:', img1.ndim)
+print('img2.ndim:', img2.ndim)
+print('img1.shape:', img1.shape)
+print('img2.shape:', img2.shape)
+print('img1.dtype:', img1.dtype)
+
+
+# 영상의 크기 참조
+h,w = img1.shape
+print('img1 size: {} x {}'.format(w, h))
+
+h, w = img2.shape[:2]
+print('img2 size: {} x {}'.format(w, h))
+
+if len(img1.shape) == 2:
+    print('img1 is a grayscale image')
+elif len(img1.shape) == 3:
+    print('img1 is a truecolor image')
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.waitKey()
+
+# 영상의 픽셀 값 참조
+x = 20
+y =10
+
+p1 = img1[y,x]
+print('p1',p1)
+
+p2 = img2[y,x]
+print('p2',p2)
+
+for y in range(h):
+    for x in range(w):
+        img1[y, x] = 0
+        img2[y, x] = (0, 200, 0)        
+
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
---
+++ ch02/img_ops.py
@@ -0,0 +1,44 @@
+﻿import numpy as np
+import cv2
+
+
+# 새 영상 생성하기
+img1 = np.empty((240, 320), dtype=np.uint8)       
+img2 = np.zeros((240, 320, 3), dtype=np.uint8)    
+img3 = np.ones((240, 320), dtype=np.uint8) * 255  # whilte
+img4 = np.full((240, 320, 3), (200, 0 , 200), dtype=np.uint8)  # yellow
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.imshow('img4', img4)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# 영상 복사
+img1 = cv2.imread('ch02\\flower2.PNG')
+
+img2 = img1
+img3 = img1.copy()
+
+img1.fill(255)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# 부분 영상 추출
+img1 = cv2.imread('ch02\\flower2.PNG')
+
+img2 = img1[20:120, 30:250]  # numpy.ndarray의 슬라이싱
+img3 = img1[20:120, 30:250].copy()
+
+cv2.circle(img2, (50,50), 20, (0,0,255),2)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('img3', img3)
+cv2.waitKey()
+cv2.destroyAllWindows()
